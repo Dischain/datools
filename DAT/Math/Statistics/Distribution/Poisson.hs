ca1037bad2f530ced2e804b70343f04eb6d65ff1
dischain <rdischain@yandex.ru>
2019-02-06 18:27:30 +0300

    add poisson distribution

--- DAT/Math/Statistics/Distribution/Poisson.hs
+++ DAT/Math/Statistics/Distribution/Poisson.hs
@@ -10,17 +10,25 @@ instance Dist.Distribution PoissonDistribution where
     | x' < 0 = 0
     | isInfinite x = 1
     | isNaN x = error $ errMsg "x is NaN"
-    | otherwise = sum' lambda x
+    | otherwise = e * (sum' lambda x')
       where
         sum' l k
-          | k == 0 = lambda
-          | otherwise = ((exp 1) ** (- l)) * ((l ** k)/ (fact' k) + (sum' l (k - 1)))
-          -- | otherwise =  fact' k
+          | k == 0 = 0
+          | otherwise = (l ** (fromIntegral k) / fact' (fromIntegral k)) + sum' l (k - 1)
         x' = floor x :: Int
-        fact' n
-          | n == 0 = 1
-          | n == 1 = 1
-          | otherwise = n * (fact' $ n - 1)
+        e = (exp 1 ) ** (- lambda)
 
+instance Dist.DiscreteDistribution PoissonDistribution where
+  prob (PoissonDist lambda) k
+    | k < 1 = 0
+    | otherwise = ((lambda ** (fromIntegral k)) * e) / (fromIntegral $ fact' k)
+      where
+        e = (exp 1 ) ** (- lambda)
+
+fact' n
+  | n == 0 = 1
+  | n == 1 = 1
+  | otherwise = n * (fact' $ n - 1)
+  
 errMsg :: String -> String
 errMsg str = "DAT.Math.Statistics.Distribution.Poisson: " ++ str
