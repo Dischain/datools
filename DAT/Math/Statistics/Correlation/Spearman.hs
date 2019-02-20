320fcf567677ec3a9fb8097699ae39fb3d2bc4ad
dischain <rdischain@yandex.ru>
2019-02-20 16:07:34 +0300

    refactored

--- DAT/Math/Statistics/Correlation/Spearman.hs
+++ DAT/Math/Statistics/Correlation/Spearman.hs
@@ -3,31 +3,22 @@ module DAT.Math.Statistics.Correlation.Spearman
   spearman
 ) where
 
-import DAT.Math.Statistics.Test.Rank hiding (rankedSample, rankedMap)
+import DAT.Math.Statistics.Test.Rank
 import DAT.Row hiding (fromList)
 import DAT.Table
 import Data.HashMap hiding (toList)
 import Data.Hashable
 
 spearman :: (Eq a, Ord a, Fractional a, Hashable a) => 
-            Table a -> Double
-spearman t
-  | numRows t /= 2 = error $ errMsg "should be provided only two samples"
-  | ni 0 /= ni 1 = error $ errMsg "samples should be of equal length"
+            Row a -> Row a -> Double
+spearman r1 r2
+  | lengthR r1 /= lengthR r2 = error $ errMsg "samples should be of equal length"
   | otherwise = 1 - (6 * d2) / (n * (n ** 2 - 1))
     where        
-      d2 = foldl (+) 0 (fmap (\(Rank r) -> r ** 2) ((-) <$> r1 <*> r2))
-      
-      r1 = rankedRow (ri 0)
-      r2 = rankedRow (ri 1)
+      d2 = foldl (+) 0 
+                 (fmap (\(Rank r) -> r ** 2) ((-) <$> (rankedRow r1) <*> (rankedRow r2)))      
 
-      ri i = case ithRow i t of
-        (Just (ConsT r Empty)) -> r
-        _ -> Row []
-
-      ni i = lengthR $ ri i
-
-      n = fromIntegral $ ni 0
+      n = fromIntegral $ lengthR r1
 
 rankedRow :: (Eq a, Ord a, Fractional a, Hashable a) => 
               Row a -> Row Rank
--- test/DAT/Math/Statistics/Correlation/SpearmanSpec.hs
+++ test/DAT/Math/Statistics/Correlation/SpearmanSpec.hs
@@ -9,7 +9,10 @@ spec :: Spec
 spec = do
   describe "Spearman" $ do
     it "should compute Spearman correlation" $ do
-      spearman t `shouldBe` 0.6428571428571428
+      spearman r1 r2 `shouldBe` 0.6428571428571428
 
-t :: Table Double
-t = ConsT (Row [79, 76, 78, 65, 86, 82, 91]) (ConsT (Row [77, 78, 79, 80, 86, 89, 91]) Empty)
+r1 :: Row Double
+r1 = Row [79, 76, 78, 65, 86, 82, 91]
+
+r2 :: Row Double
+r2 = Row [77, 78, 79, 80, 86, 89, 91]
